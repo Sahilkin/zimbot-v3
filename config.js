1a2112967b06afd36fb4bdd3ada85fd5cd3f5732
Sahilkin <129608144+Sahilkin@users.noreply.github.com>
2024-01-10 07:29:19 +0500

    Update config.js
    Signed-off-by: Sahilkin <129608144+Sahilkin@users.noreply.github.com>

--- config.js
+++ config.js
@@ -37,13 +37,13 @@ global.APIKeys = {
 }
 
 // Other
-global.owner = ['27634090203','']
-global.premium = ['27634090203','918188019676']
-global.botname = 'ZIM BOT INC'
-global.ownername = 'DRIPS'
-global.group1 = 'https://chat.whatsapp.com/EFsb8RCXV4jLEFk4eAcA1A'
-global.group2 = 'https://chat.whatsapp.com/HSfcYU13g5C8GxINWwSWoA'
-global.packname = 'ZIM BOT INC'
+global.owner = [923175013358','']
+global.premium = [923175013358',']
+global.botname = 'SAHIL BOT INC'
+global.ownername = 'SAHIL KING'
+global.group1 = 'https://whatsapp.com/channel/0029VaFv1RZ3QxRtLXfI993h'
+global.group2 = 'https://chat.whatsapp.com/CDWzBxhrB4uEmDEavQjT70'
+global.packname = 'SAHIL KING 923201151138'
 global.ytchannel = 'https://youtu.be/ww4z2m3uORU'
 global.monayawal = '100000'
 global.darahawal = '100'
@@ -65,10 +65,10 @@ global.f3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.docu
 global.f4 = 'application/zip'
 global.f5 = 'application/pdf'
 global.f6 = 'application/vnd.android.package-archive'
-global.author = 'ZIM BOT INC'
+global.author = 'SAHIL KING 923201151138'
 global.flaming = 'https://flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=amped-logo&doScale=true&scaleWidth=800&scaleHeight=500&text='
 global.sessionName = 'session'
-global.prefa = ['','!','.','🐦','🐤','🗿']
+global.prefa = ['','!','.','🐦','🐤','+']
 global.sp = '⭔'
 global.mess = {
     success: '*success whoa*',
